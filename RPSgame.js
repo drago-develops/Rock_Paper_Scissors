52d939f8da15ec813b5849416e04741bf56bcbc9
drago-develops <ambro.rot@gmail.com>
2024-06-02 14:51:35 +0100

    added conditions to check if players entered valid
    to be finised as funtion only return else statement

--- RPSgame.js
+++ RPSgame.js
@@ -1,5 +1,14 @@
 function getHumanChoice(){
     var humanChoice = prompt("Please choose either Rock, Paper or Sciccors");
-    return humanChoice
+    if (humanChoice.toUpperCase === "ROCK"){
+        return humanChoice;
+    } else if (humanChoice.toUpperCase === "PAPER"){
+        return humanChoice;
+    } else if(humanChoice.toUpperCase === "SCISSORS"){
+        return humanChoice;
+    } else {
+        alert("Please enter either Rock, Paper or Scissors. Please check your spelling")
+    }
+    
 }
 console.log(getHumanChoice());
