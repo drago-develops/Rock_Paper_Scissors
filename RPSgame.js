eedf0063b6179c59cf2a4762769088f6d79dbd1b
drago-develops <ambro.rot@gmail.com>
2024-06-03 14:59:40 +0100

    added var Scores and logic function to play game
    created two varibles to keep track of the score
    added function with if, else if and else to deterimine
    winner of a round of game. Function will output how the
    round was won and update the score to a variable.

--- RPSgame.js
+++ RPSgame.js
@@ -1,3 +1,6 @@
+var humanScore = 0;
+var computerScore = 0;
+
 function getHumanChoice(){
     const request = prompt("Please choose either Rock, Paper or Sciccors");
     var humanChoice = request.toUpperCase();
@@ -23,4 +26,28 @@ function getComputerChoice(){
     } else if (computerChoice === 3){
         return "SCISSORS"
     };
+}
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === "ROCK" && computerChoice === "SCISSORS"){
+        console.log("Rock beats Scissors! You win!")
+        humanScore ++
+    } else if (humanChoice === "PAPER" && computerChoice === "ROCK"){
+        console.log("Paper beats Rock! You win!")
+        humanScore ++
+    } else if (humanChoice === "SCISSORS" && computerChoice === "PAPER"){
+        console.log("Scissors beats Paper! You win!")
+        humanScore ++
+    } else if (computerChoice === "ROCK" && humanChoice === "SCISSORS"){
+        console.log("Rock beats Scissors! You loose!")
+        computerScore ++
+    } else if (computerChoice === "PAPER" && humanChoice === "ROCK"){
+        console.log("Paper beats Rock! You loose!")
+        computerScore ++
+    } else if (computerChoice === "SCISSORS" && humanChoice === "PAPER"){
+        console.log("Scissors beats Paper! You loose!")
+        computerScore ++
+    } else{
+        console.log("You drew same hand as computer, no score.")
+    }
 }
