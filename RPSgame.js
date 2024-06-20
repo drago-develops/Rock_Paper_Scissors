c334d8ac8007b049df537ee765017e330f3514a5
drago-develops <ambro.rot@gmail.com>
2024-06-20 16:38:01 +0100

    eventListeners to buttoms
    added eventListeners for each of the bottom calling playRound()
    and correct player selection for eac buttons.

--- RPSgame.js
+++ RPSgame.js
@@ -1,65 +1,77 @@
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", ()=>{
+    playRound("rock",getComputerChoice())
+});
 
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", ()=>{
+    playRound("paper",getComputerChoice())
+});
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", ()=>{
+    playRound("scissors",getComputerChoice())
+});
 
 function playGame(){
     var humanScore = 0;
     var computerScore = 0;
     
-    let i = 1;
-    while (i<=5){
-        function getHumanChoice(){
-            const request = prompt("Please choose either Rock, Paper or Scissors");
-            var humanChoice = request.toUpperCase();
-            if (humanChoice === "ROCK"){
-                return humanChoice;
-            } else if (humanChoice === "PAPER"){
-                return humanChoice;
-            } else if(humanChoice  === "SCISSORS"){
-                return humanChoice;
-            } else {
-                alert("Please enter either Rock, Paper or Scissors. Please check your spelling");
-                return getHumanChoice();
-            }
-            
+    
+    function getHumanChoice(){
+        const request = prompt("Please choose either Rock, Paper or Scissors");
+        var humanChoice = request.toUpperCase();
+        if (humanChoice === "ROCK"){
+            return humanChoice;
+        } else if (humanChoice === "PAPER"){
+            return humanChoice;
+        } else if(humanChoice  === "SCISSORS"){
+            return humanChoice;
+        } else {
+            alert("Please enter either Rock, Paper or Scissors. Please check your spelling");
+            return getHumanChoice();
+        }
+        
+    };
+    function getComputerChoice(){
+        const computerChoice = Math.floor((Math.random()*3)+1);
+        if (computerChoice ===1){
+            return "ROCK"
+        } else if (computerChoice === 2){
+            return "PAPER"
+        } else if (computerChoice === 3){
+            return "SCISSORS"
         };
-        function getComputerChoice(){
-            const computerChoice = Math.floor((Math.random()*3)+1);
-            if (computerChoice ===1){
-                return "ROCK"
-            } else if (computerChoice === 2){
-                return "PAPER"
-            } else if (computerChoice === 3){
-                return "SCISSORS"
-            };
-        };
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
+    };
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
 
-        function playRound(humanChoice, computerChoice){
-            if (humanChoice === "ROCK" && computerChoice === "SCISSORS"){
-                console.log("Rock beats Scissors! You win!")
-                humanScore ++
-            } else if (humanChoice === "PAPER" && computerChoice === "ROCK"){
-                console.log("Paper beats Rock! You win!")
-                humanScore ++
-            } else if (humanChoice === "SCISSORS" && computerChoice === "PAPER"){
-                console.log("Scissors beats Paper! You win!")
-                humanScore ++
-            } else if (computerChoice === "ROCK" && humanChoice === "SCISSORS"){
-                console.log("Rock beats Scissors! You loose!")
-                computerScore ++
-            } else if (computerChoice === "PAPER" && humanChoice === "ROCK"){
-                console.log("Paper beats Rock! You loose!")
-                computerScore ++
-            } else if (computerChoice === "SCISSORS" && humanChoice === "PAPER"){
-                console.log("Scissors beats Paper! You loose!")
-                computerScore ++
-            } else{
-                console.log("You drew same hand as computer, no score.")
-            }
-        };
-        playRound(humanSelection, computerSelection);
-    i++;
-    }
+    function playRound(humanChoice, computerChoice){
+        if (humanChoice === "ROCK" && computerChoice === "SCISSORS"){
+            console.log("Rock beats Scissors! You win!")
+            humanScore ++
+        } else if (humanChoice === "PAPER" && computerChoice === "ROCK"){
+            console.log("Paper beats Rock! You win!")
+            humanScore ++
+        } else if (humanChoice === "SCISSORS" && computerChoice === "PAPER"){
+            console.log("Scissors beats Paper! You win!")
+            humanScore ++
+        } else if (computerChoice === "ROCK" && humanChoice === "SCISSORS"){
+            console.log("Rock beats Scissors! You loose!")
+            computerScore ++
+        } else if (computerChoice === "PAPER" && humanChoice === "ROCK"){
+            console.log("Paper beats Rock! You loose!")
+            computerScore ++
+        } else if (computerChoice === "SCISSORS" && humanChoice === "PAPER"){
+            console.log("Scissors beats Paper! You loose!")
+            computerScore ++
+        } else{
+            console.log("You drew same hand as computer, no score.")
+        }
+    };
+    playRound(humanSelection, computerSelection);
+    
+    
     function gameResults(){
         if (humanScore>computerScore){
             console.log(`Congratulations, you've won! Your score is ${humanScore} and computer score is ${computerScore}.`);
